4fb814c244828aeffb0734cc088ebdfe97efc177
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-04-05 14:47:47 +0100

    most part 6 completed

--- src/controllers/ticket.js
+++ src/controllers/ticket.js
@@ -1,30 +1,27 @@
 const prisma = require("../utils/prisma");
 
 const createTicket = async (req, res) => {
-  const { name, phone, email } = req.body;
+  const { screeningId, customerId } = req.body;
 
-    const createdTicket = await prisma.ticket.create({
+  const createdTicket = await prisma.ticket.create({
     data: {
-      name,
-      contact: {
-        create: {
-          phone,
-          email,
-        },
+      screening: {
+        connect: { id: parseInt(screeningId) },
+      },
+      customer: {
+        connect: { id: parseInt(customerId) },
       },
     },
-    // We add an `include` outside of the `data` object to make sure the new contact is returned in the result
-    // This is like doing RETURNING in SQL
+
     include: {
-      contact: true,
-    },
+      screening: true,
+      customer: true,
+      },
   });
 
   res.json({ data: createdTicket });
 };
 
-
-
 module.exports = {
-  createTicket
+  createTicket,
 };
--- src/routers/ticket.js
+++ src/routers/ticket.js
@@ -5,11 +5,6 @@ const {
 
 const router = express.Router();
 
-// In index.js, we told express that the /customer route should use this router file
-// The below /register route extends that, so the end result will be a URL
-// that looks like http://localhost:4000/customer/register
 router.post("/", createTicket);
-
-
 
 module.exports = router;
